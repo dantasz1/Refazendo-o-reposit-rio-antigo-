62fdd84e58159ee464093206edfecd73fe620ab2
André Luiz <dantasluizandre@gmail.com>
2023-11-21 00:43:31 -0300

    fazendo o codigo js e colocando scripts

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ const form = e.currentTarget
     const data = {
     nome : form.nome.value
 }
+console.log(form.nome.value)
 })
-console.log(data)
+
+
+
+document.addEventListener('keypress', function (e) {
+    if (e.key === "Enter") 
+
+    var btn = document.querySelector("#clicar")
+            btn.click();
+}) 
